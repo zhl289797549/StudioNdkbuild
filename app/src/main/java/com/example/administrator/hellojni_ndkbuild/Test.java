1eae31da5f26143287b755a3775f191f1e007b84
zhenghonglin <zhenghonglin@felink.com>
2017-12-01 15:02:23 +0800

    commit Test.java

---
+++ app/src/main/java/com/example/administrator/hellojni_ndkbuild/Test.java
@@ -0,0 +1,20 @@
+package com.example.administrator.hellojni_ndkbuild;
+
+/**
+ * Created by Administrator on 2017/12/1.
+ */
+
+public class Test {
+
+
+    int id = 10001;
+
+    public static void main(String[] args){
+        System.out.println("==================");
+    }
+
+
+    public void fun1(){
+        System.out.println("==================11111111111111111");
+    }
+}
