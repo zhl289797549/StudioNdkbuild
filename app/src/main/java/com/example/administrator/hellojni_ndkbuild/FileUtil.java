a40e14abaf5254c924bee89bfbab3658cf757805
zhenghonglin <zhenghonglin@felink.com>
2017-12-01 15:06:35 +0800

    commit

---
+++ app/src/main/java/com/example/administrator/hellojni_ndkbuild/FileUtil.java
@@ -0,0 +1,11 @@
+package com.example.administrator.hellojni_ndkbuild;
+
+/**
+ * Created by Administrator on 2017/12/1.
+ */
+
+public class FileUtil {
+    public void read(){
+        System.out.println("FileUtil read");
+    }
+}
